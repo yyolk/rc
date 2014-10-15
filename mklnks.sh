ade8799034d302e3c167d7dd5117f449e96189a0
Joseph Yølk Chiocchi <joe@yolk.cc>
2014-10-15 11:41:04 -0500

    update mklinks

--- mklnks.sh
+++ mklnks.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-ln -s bashrc $HOME/.bashrc
-ln -s bash_profile $HOME/.bash_profile
+RC_DIR=~/rc
+
+ln -s $(RC_DIR)/bashrc $HOME/.bashrc
+ln -s $(RC_DIR)/bash_profile $HOME/.bash_profile
+ln -s $(RC_DIR)/bash_logout $HOME/.bash_logout
 echo "Done"
