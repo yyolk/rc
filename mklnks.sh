1be9620ad7069faa12b05ac1f59d89f44bd9b2a8
Joseph Yølk Chiocchi <input.output@gmail.com>
2015-01-11 16:07:27 -0600

    fix mklinks.sh

--- mklnks.sh
+++ mklnks.sh
@@ -2,7 +2,7 @@
 
 RC_DIR=~/rc
 
-ln -s $(RC_DIR)/bashrc $HOME/.bashrc
-ln -s $(RC_DIR)/bash_profile $HOME/.bash_profile
-ln -s $(RC_DIR)/bash_logout $HOME/.bash_logout
+ln -s $RC_DIR/bashrc $HOME/.bashrc
+ln -s $RC_DIR/bash_profile $HOME/.bash_profile
+ln -s $RC_DIR/bash_logout $HOME/.bash_logout
 echo "Done"
