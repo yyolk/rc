9327cfe4759b4d286499cf52a973904d65b3e5a9
Joseph Yølk Chiocchi <joe@yolk.cc>
2016-10-17 17:51:09 -0500

    fix ws

--- mklnks.sh
+++ mklnks.sh
@@ -5,11 +5,11 @@ RC_DIR=~/rc
 linkme ()
 {
     if [ ! -e $HOME/."$1" ] ; then
-	    echo "Linking $RC_DIR/$1..."
-	    ln -s $RC_DIR/"$1" $HOME/."$1"
-	    echo 'Linked $1 to $HOME/.'"$1"
+        echo "Linking $RC_DIR/$1..."
+        ln -s $RC_DIR/"$1" $HOME/."$1"
+        echo 'Linked $1 to $HOME/.'"$1"
     else
-            echo "Skipped linking $1, $HOME/.$1 already exists"
+        echo "Skipped linking $1, $HOME/.$1 already exists"
     fi
 }
 
