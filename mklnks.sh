d30329990f40dd5c48ad00351ed16653eeb5159b
Joseph Yølk Chiocchi <joe@yolk.cc>
2016-10-17 17:42:53 -0500

    no dry-run

--- mklnks.sh
+++ mklnks.sh
@@ -6,7 +6,7 @@ linkme ()
 {
     if [ ! -e $HOME/."$1" ] ; then
 	    echo "Linking $RC_DIR/$1..."
-	    echo ln -s $RC_DIR/"$1" $HOME/."$1"
+	    ln -s $RC_DIR/"$1" $HOME/."$1"
 	    echo 'Linked $1 to $HOME/.'"$1"
     else
             echo "Skipped linking $1, $HOME/.$1 already exists"
