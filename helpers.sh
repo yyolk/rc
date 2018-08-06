0b5d87d2a2c20d1756370b0772d898213009e967
Joseph Yølk Chiocchi <joe@yolk.cc>
2018-08-06 00:07:53 -0500

    note to self

--- helpers.sh
+++ helpers.sh
@@ -5,7 +5,11 @@
 #
 # }
 
+# bash -c 'declare -f fun'
 # export -f sum_fun
-# bash -c 'declare -f foo'
+# bash -c 'declare -f fun'
+# ..function outputs because functions must be explicitly exported to be
+# visible to sub shells
+
 
 #exit 0
