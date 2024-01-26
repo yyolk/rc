186c55aa713154c3a9271cd1934ef57a522e180c
Joseph Chiocchi <joseph.chiocchi@blastpoint.com>
2024-01-26 12:32:49 -0600

    init seeyouspacecowboy from https://gist.github.com/yyolk/ba07f96e6a289e4e6fde21feab4f0bbb

---
+++ get_bash_logout_banner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+get_bash_logout()
+{
+  #seeyouspacecowboy
+  curl -L https://gist.githubusercontent.com/yyolk/ba07f96e6a289e4e6fde21feab4f0bbb/raw/2a1017b92335aacd72018b9e312a294cc6a0e73c/seeyouspacecowboy.sh -o seeyouspacecowboy.sh
+  chmod +x seeyouspacecowboy.sh
+}
+
+get_bash_logout
