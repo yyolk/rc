4a49f6942c8e6185418146ba31a286715e49c9e9
Joseph Chiocchi <joseph.chiocchi@blastpoint.com>
2024-01-29 10:40:31 -0600

    update gen_brewfile.sh

--- gen_brewfile.sh
+++ gen_brewfile.sh
@@ -5,18 +5,25 @@
 # Usage:
 #    ./gen_brewfile.sh > Brewfile.output
 
-echo "# Install with `brew bundle`"
-echo 
+
+echo "#"
+echo "# Install with brew bundle -f Brewfile.output"
+echo "#"
+
+echo "# Taps"
+echo """# brew tap | awk '{printf("tap \\"%s\\"\\n", \$0)}'"""
+brew tap | awk '{printf("tap \"%s\"\n", $0)}'
+
 echo "# Formula"
-echo """# brew list --formula -1 | awk '{printf(brew "%s"\\n, \$0)}'"""
-brew list --formula -1 | awk '{printf("brew %s\n", $0)}'
+echo """# brew list --formula -1 | awk '{printf("brew \\"%s\\"\\n", \$0)}'"""
+brew list --formula -1 | awk '{printf("brew \"%s\"\n", $0)}'
 
 echo
 echo "# Casks"
-echo """# brew list --formula -1 | awk '{printf(cask "%s"\\n,)}'"""
+echo """# brew list --formula -1 | awk '{printf("cask \\"%s\\"\\n",)}'"""
 brew list --formula -1 | awk '{printf("cask %s\n", $0)}'
 
 echo
 echo "# mas (mac app store cli)"
-echo """# mas list | awk '{printf("mas \"%s\", id: %s\n", \$(2), \$(1))}'"""
+echo """# mas list | awk '{printf("mas \\"%s\\", id: %s\n", \$(2), \$(1))}'"""
 mas list | awk '{printf("mas \"%s\", id: %s\n", $(2), $(1))}'
