9ec31b4c1da530eed7ce37516c4f3b6f2a4b58ea
Joseph Chiocchi <joseph.chiocchi@blastpoint.com>
2024-01-31 18:04:14 -0600

    update install_bin.sh

--- install_bin.sh
+++ install_bin.sh
@@ -2,4 +2,24 @@
 
 set -xe
 
-go get -u github.com/acarl005/ls-go
+BIN_PATH=$HOME/.local/bin
+
+OS=$(uname -o | tr '[:upper:]' '[:lower:]')
+MARCH=$(uname -m)
+
+# ls-go v1.0.1
+LS_GO_VERSION=v1.0.1
+curl -L https://github.com/acarl005/ls-go/releases/download/$LS_GO_VERSION/ls-go-$OS-$MARCH -o $BIN_PATH/ls-go
+chmod +x $BIN_PATH/ls-go
+
+# case $(uname -m) in
+#   arm64)
+#     # ls-go v1.0.1
+#     LS_GO_VERSION=v1.0.1
+#     curl -L https://github.com/acarl005/ls-go/releases/download/$LS_GO_VERSION/ls-go-darwin-arm64 -o $BIN_PATH/ls-go
+#     chmod +x $BIN_PATH/ls-go
+#     ;;
+#   *)
+#     echo "No matching arch binary distributions."
+#     ;;
+# esac
